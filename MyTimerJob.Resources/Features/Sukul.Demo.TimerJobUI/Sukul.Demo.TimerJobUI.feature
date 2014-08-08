d7ed073f09ecad16521315fe6f4401e52dfd4856
shailensukul <shailensukul@gmail.com>
2014-08-08 03:59:20 +0000

    Display webpart done

---
+++ MyTimerJob.Resources/Features/Sukul.Demo.TimerJobUI/Sukul.Demo.TimerJobUI.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="5a2178c1-1ce2-43c5-96ac-65d38d6ea50e" description="Accompanying UI files for Timer Job presentation" featureId="5a2178c1-1ce2-43c5-96ac-65d38d6ea50e" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="Sukul.Demo.TimerJobUI" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="9639b0d4-a757-41ac-bc1d-2a51f4fcce4b" />
+  </projectItems>
+</feature>
---
+++ MyTimerJob.Resources/Resources/javascript/Sukul.Demo.Weather.js
@@ -0,0 +1,47 @@
+﻿if (SUKUL == undefined) var SUKUL;
+if (!SUKUL) SUKUL = {};
+
+$(document).ready(function () {
+    var weather = new SUKUL.Weather();
+    weather.ShowSummaryWeather();
+});
+
+
+SUKUL.Weather = function () {
+    var self = this;
+    self.ShowSummaryWeather = function () {
+        $.ajax({
+            url: "/_api/web/lists/GetByTitle('Yahoo%20Weather')/items/?$select=Title,JSONData&$filter=Success eq 1",
+            type: "GET",
+            headers: { "Accept": "application/json;odata=verbose" },
+            success: function (data) {
+                var listData = data.d.results;
+                var ul = $("<ul style='list-style-type: none; padding-left: 0px;' class='cbs-List'>");
+                for (i = 0; i < listData.length; i++) {
+                   
+                    var listItemData = $.parseJSON(listData[i].JSONData);
+                    var htmlStr = "<li style='display: inline;'><div style='width: 320px; float: left; display: table; margin-bottom: 10px; margin-top: 5px;'>";
+                    htmlStr += "<a href='#'>";
+                    htmlStr += "<div style='float: left; width: 140px; padding-right: 10px;'>";
+                    htmlStr += listItemData.query.results.channel.item.description;
+                    htmlStr += "</div>";
+                    htmlStr += "<div style='float: left; width: 170px'>";
+                    htmlStr += "<div class='mtcProfileHeader mtcProfileHeaderP'>" + listItemData.query.results.channel.title + "</div>";
+                    htmlStr += "</div></a></div></li>";
+                    ul.append($(htmlStr))
+                     
+                }
+                $("#divContentContainer").append(ul);
+            },
+            error: function (data) {
+                alert(data.statusText);
+            }
+        });
+    }
+
+function setImageWidth(imgString, width) {
+    var img = $(imgString);
+    img.css('width', width);
+    return img[0].outerHTML;
+}
+}
